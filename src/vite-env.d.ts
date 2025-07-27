3d3c6a4d44f00e7cff1dc47102fa1ff7383f4aa7
Steven Sinatra <steven@stevensinatra.id>
2025-07-27 15:17:26 +1000

    Dynamic cors setting

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly CORS_ORIGIN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,57 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({
-  plugins: [
-    react({
-      // plugins: [['@preact-signals/safe-react/swc', {}]]
-    }),
-    tailwindcss(),
-  ],
-  server: {
-    cors: {
-      origin: /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\]|192\.168\.1\.37)(?::\d+)?$/,
-    },
-  },
-  preview: {
-    port: 5173,
-    strictPort: true,
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        build: 'src/build.ts',
-        dev: 'src/ha-dev.ts',
+
+const CORS_DEFAULT =
+  /^https?:\/\/(?:(?:[^:]+\.)?localhost|127\.0\.0\.1|\[::1\])(?::\d+)?$/;
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const CORS_ORIGIN = env.CORS_ORIGIN?.split(',') || [];
+
+  return {
+    plugins: [
+      react({
+        // plugins: [['@preact-signals/safe-react/swc', {}]]
+      }),
+      tailwindcss(),
+    ],
+    server: {
+      cors: {
+        origin: [...CORS_ORIGIN, CORS_DEFAULT],
       },
-      output: {
-        entryFileNames: (chunkInfo) => {
-          if (chunkInfo.name === 'dev') {
-            return 'ha-dev.js';
-          }
-          return 'ha-custom-cards.js';
+    },
+    preview: {
+      port: 5173,
+      strictPort: true,
+      cors: {
+        origin: [...CORS_ORIGIN, CORS_DEFAULT],
+      },
+    },
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: {
+          build: 'src/build.ts',
+          dev: 'src/ha-dev.ts',
         },
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[hash].[ext]`,
+        output: {
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === 'dev') {
+              return 'ha-dev.js';
+            }
+            return 'ha-custom-cards.js';
+          },
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[hash].[ext]`,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
